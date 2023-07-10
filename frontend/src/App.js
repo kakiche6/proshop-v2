6fa3957fa1e8b2438bb63ffda25bea5fd009e2c1
Arezki <mmezrag@techitaway.co>
2023-07-10 19:21:23 +0100

    header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,16 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Welcome to proshop</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <h1>Welcome to proshop</h1>
+      </main>
+      <Footer />
+    </>
+  );
 };
 export default App;
