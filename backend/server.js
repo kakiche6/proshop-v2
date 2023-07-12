7562ff104104bbd4de673267bda4efd9c66c52b9
Arezki <mmezrag@techitaway.co>
2023-07-12 15:01:58 +0100

    JWT http-only cookie

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
