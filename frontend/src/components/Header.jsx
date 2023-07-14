50a74940cda705256870a78e3106460334d177ad
Arezki <mmezrag@techitaway.co>
2023-07-14 12:04:29 +0100

    list users admin

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -77,6 +77,9 @@ const Header = () => {
                   <LinkContainer to='/admin/productlist'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
                 </NavDropdown>
               )}
             </Nav>
